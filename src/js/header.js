b050b0ead449049baa152932b7151a27428afc5b
coutinhojessica <jessicamaiara.coutinho@gmail.com>
2026-07-03 21:27:15 -0300

    feat: concluída a seccão header da página

--- src/js/header.js
+++ src/js/header.js
@@ -1,1 +1,7 @@
-
+export function iniciarHeader() {
+    const btnMobile = document.querySelector('.iconMobile');
+    let navBar = document.querySelector('.navBar');
+    btnMobile.addEventListener('click', () => {
+        navBar.classList.toggle('activeMenu');
+    })
+}
--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,16 @@
+import { iniciarHeader } from "./header.js";
 
+async function carregarComponentes(id, arquivo) {
+  const resposta = await fetch(arquivo);
+  const html = await resposta.text();
+
+  document.getElementById(id).innerHTML = html;
+
+  if (id === "header") {
+    iniciarHeader();
+  }
+}
+
+carregarComponentes("header", "./components/header.html");
+carregarComponentes("hero", "./components/hero.html");
+carregarComponentes("footer", "./components/footer.html");
